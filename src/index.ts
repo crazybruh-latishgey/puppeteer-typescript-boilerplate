a1f4a74c9d845ad3395b86097366db5b31bf772e
crazybruh-latishgey <74991042+crazybruh-latishgey@users.noreply.github.com>
2020-11-25 00:37:16 +0300

    g

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,2 @@
-import puppeteer from 'puppeteer';
-
-(async () => {
-  const browser = await puppeteer.launch({ headless: false });
-
-  const page = await browser.newPage();
-
-  await page.goto('https://google.com');
-
-  browser.close();
-})();
+нига тзат булщит...
+bruh
